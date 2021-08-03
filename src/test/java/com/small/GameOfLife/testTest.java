dec77aa99d875f2b14ed9e06d2490331d0e1c6a0
Bradley M. Small <bradley_small@hotamil.com>
2021-08-03 15:46:34 -0400

    placeholder test file

---
+++ src/test/java/com/small/GameOfLife/testTest.java
@@ -0,0 +1,13 @@
+package test.java.com.small.GameOfLife;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class testTest {
+    @Test
+    void shouldSayHello() {
+        assertEquals(0, 1);
+    }
+}
